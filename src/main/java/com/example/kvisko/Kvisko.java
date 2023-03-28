b7d4be5bf3f2d995a7eb3a773f232cb41e727397
kele-b <b.kelecevic97@gmail.com>
2023-03-28 14:14:14 +0200

    populating database with questions added

--- src/main/java/com/example/kvisko/Kvisko.java
+++ src/main/java/com/example/kvisko/Kvisko.java
@@ -8,8 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class Kvisko extends Application {
@@ -20,17 +19,57 @@ public class Kvisko extends Application {
 
     @Override
     public void init() {
-        //Establishing a connection with the database
+        List<Question> questions = ReadXMLFile.getQuestions();
+
+        // establishing a connection with the database
         String url = "jdbc:mysql://localhost:3306/mydatabase?createDatabaseIfNotExist=true";
         String username = "root";
         String password = "admin123";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            DriverManager.getConnection(url, username, password);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection is successful to the database " + url);
+
+            // create the questions table if it doesn't exist
+            Statement createTableStatement = connection.createStatement();
+            String createTableQuery =
+                    "CREATE TABLE IF NOT EXISTS questions (id INT PRIMARY KEY AUTO_INCREMENT, " +
+                            "question_text VARCHAR(1000), " +
+                            "correct_answer VARCHAR(255)," +
+                            "answer_1 VARCHAR(255)," +
+                            "answer_2 VARCHAR(255)," +
+                            "answer_3 VARCHAR(255))";
+            createTableStatement.execute(createTableQuery);
+
+            // check if the questions table is already populated
+            Statement selectStatement = connection.createStatement();
+            String selectQuery = "SELECT * FROM questions";
+            ResultSet resultSet = selectStatement.executeQuery(selectQuery);
+
+            if (!resultSet.next()) {
+
+                // insert data into the question table if is not populated
+                PreparedStatement insertStatement = connection.prepareStatement
+                        ("INSERT INTO questions (question_text, correct_answer, answer_1, answer_2, answer_3) " +
+                                "VALUES (?, ?, ?, ?, ?)");
+                for (Question q : questions) {
+                    insertStatement.setString(1, q.getQuestionText());
+                    insertStatement.setString(2, q.getCorrectAnswer());
+                    for (int i = 0; i < q.getAnswers().size(); i++) {
+                        insertStatement.setString(i + 3, q.getAnswers().get(i));
+                    }
+                    insertStatement.executeUpdate();
+                }
+            }
+
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
         }
-    }
+
+}
 
     @Override
     public void start(Stage stage) throws Exception {
